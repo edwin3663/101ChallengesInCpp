bd434fa3b666401660ce0ae9cdd6d7cde433e8a8
edwin3663 <pgsaffold@hotmail.com>
2020-10-25 10:24:37 -0700

    Oct 25, 2020

---
+++ chall13.cpp
@@ -0,0 +1,27 @@
+/** Computes the Cardano Triplits that exist for an a+b+c <= 100
+    @file
+    @author Peter Saffold
+    @date
+*/
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char* argv[]) {
+
+    for (int a = 0; a <= 100; a++) {
+        for (int b = 0; b <= 100; b++) {
+            for (int c = 0; c <= 100; c++) {
+                if (a + b + c <= 100) {
+                    double b_c = b * sqrt(c);
+                    double sum = cbrt(a+b_c) + cbrt(a-b_c);
+                    if (sum < 1.000001 && sum > 0.999999) {
+                        std::cout << "(" << a << "," << b << "," << c << ")" << std::endl;
+                    }
+                }
+            }
+        }
+    }
+    
+
+    return 0;
+}
